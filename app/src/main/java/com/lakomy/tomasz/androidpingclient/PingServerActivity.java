201198133209fa086a82425b57922fc64b01330e
Tomasz Lakomy <tomaszlakomy@yahoo.com>
2015-08-08 14:13:28 +0200

    Layout redesign

--- app/src/main/java/com/lakomy/tomasz/androidpingclient/PingServerActivity.java
+++ app/src/main/java/com/lakomy/tomasz/androidpingclient/PingServerActivity.java
@@ -1,9 +1,11 @@
 package com.lakomy.tomasz.androidpingclient;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 
@@ -42,5 +44,10 @@ public class PingServerActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void pingServer(View view) {
+        TextView info = (TextView) findViewById(R.id.ping_info);
+        info.setText("Button pressed");
     }
 }
