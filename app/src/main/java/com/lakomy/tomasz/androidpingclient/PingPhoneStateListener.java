8955d8b83d619901a16a7dc87e93b95eeb7b5ccb
Tomasz Lakomy <tomaszlakomy@yahoo.com>
2016-11-09 18:20:16 +0100

    Added measuring signal strength

--- app/src/main/java/com/lakomy/tomasz/androidpingclient/PingPhoneStateListener.java
+++ app/src/main/java/com/lakomy/tomasz/androidpingclient/PingPhoneStateListener.java
@@ -1,24 +1,19 @@
 package com.lakomy.tomasz.androidpingclient;
 
-import android.telephony.CellInfo;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
 import android.util.Log;
-import android.widget.TextView;
-
-import java.util.List;
 
 class PingPhoneStateListener extends PhoneStateListener {
-    public int signalStrengthValue = 0;
 
     @Override
     public void onSignalStrengthsChanged(SignalStrength signalStrength) {
         super.onSignalStrengthsChanged(signalStrength);
-//        Log.d("aping", "isGSM: " + signalStrength.isGsm());
-//        Log.d("aping", "getCdmaDbm: " + signalStrength.getCdmaDbm() + " dBm");
-//        Log.d("aping", "getEvdoDbm: " + signalStrength.getEvdoDbm() + " dBm");
-//        Log.d("aping", "getGsmSignalStrength: " + signalStrength.getGsmSignalStrength() + " dBm");
-//        Log.d("aping", "getGsmSignalStrength: " + signalStrength.getGsmBitErrorRate());
+        int gsmSignalStrength = signalStrength.getGsmSignalStrength();
+
+        PingServerActivity.signalStrength =
+                gsmSignalStrength != 99 ? -113 + 2 * gsmSignalStrength : -120;
+
     }
 
 }
--- app/src/main/java/com/lakomy/tomasz/androidpingclient/PingServerActivity.java
+++ app/src/main/java/com/lakomy/tomasz/androidpingclient/PingServerActivity.java
@@ -54,6 +54,7 @@ public class PingServerActivity extends FragmentActivity
 
     static public int numberOfRequests;
     static public int sumOfRequestTimes;
+    static public int signalStrength;
     static public double averageRequestTime;
     static public double medianRequestTime;
     static public double quartileDeviation;
@@ -242,6 +243,7 @@ public class PingServerActivity extends FragmentActivity
             textView.setText("Sending " + numberOfPackets + " packets"
                     + "\nCurrent network: \t" + currentNetworkType
                     + "\nRequest number: \t#" + numberOfRequests
+                    + "\nCurrent signal strength: \t" + signalStrength + " dbM"
                     + "\nCurrent request time: \t" + lastKnownDeltaTime
                     + "\nMedian request time: \t" + medianRequestTime
                     + "\nQuartile deviation: \t" + quartileDeviation
@@ -254,19 +256,13 @@ public class PingServerActivity extends FragmentActivity
     public static String getCurrentNetworkType() {
         int networkType = telephonyManager.getNetworkType();
         switch (networkType) {
-            case TelephonyManager.NETWORK_TYPE_1xRTT: return "1xRTT";
             case TelephonyManager.NETWORK_TYPE_CDMA: return "CDMA";
             case TelephonyManager.NETWORK_TYPE_EDGE: return "EDGE";
-            case TelephonyManager.NETWORK_TYPE_EHRPD: return "eHRPD";
-            case TelephonyManager.NETWORK_TYPE_EVDO_0: return "EVDO rev. 0";
-            case TelephonyManager.NETWORK_TYPE_EVDO_A: return "EVDO rev. A";
-            case TelephonyManager.NETWORK_TYPE_EVDO_B: return "EVDO rev. B";
             case TelephonyManager.NETWORK_TYPE_GPRS: return "GPRS";
             case TelephonyManager.NETWORK_TYPE_HSDPA: return "HSDPA";
             case TelephonyManager.NETWORK_TYPE_HSPA: return "HSPA";
             case TelephonyManager.NETWORK_TYPE_HSPAP: return "HSPA+";
             case TelephonyManager.NETWORK_TYPE_HSUPA: return "HSUPA";
-            case TelephonyManager.NETWORK_TYPE_IDEN: return "iDen";
             case TelephonyManager.NETWORK_TYPE_LTE: return "LTE";
             case TelephonyManager.NETWORK_TYPE_UMTS: return "UMTS";
             case TelephonyManager.NETWORK_TYPE_UNKNOWN: return "Unknown network";
@@ -297,7 +293,6 @@ public class PingServerActivity extends FragmentActivity
 
     public void performHttpRequests() {
         final TextView pingInfo = (TextView) findViewById(R.id.ping_info);
-        Log.d("aping", "Creating a http request task: " + url);
         HttpRequestTask httpRequestTask = new HttpRequestTask(url, packetSize, pingInfo);
         RequestTask task = new RequestTask(httpRequestTask.getStringRequest(), queue);
 
